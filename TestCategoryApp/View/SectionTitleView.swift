b0aea8f231870754b360f22d3102048cd73f1eea
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2023-01-20 15:59:31 +0530

    text used for textfield

--- TestCategoryApp/View/SectionTitleView.swift
+++ TestCategoryApp/View/SectionTitleView.swift
@@ -5,4 +5,22 @@
 //  Created by Bhumika Patel on 20/01/23.
 //
 
-import Foundation
+struct SectionTitleView: View {
+    var title: String
+    
+    var body: some View {
+        HStack {
+            Text(title)
+                .font(.system(size: 24))
+                .bold()
+            Spacer()
+        }
+    }
+}
+
+struct SectionTitleView_Previews: PreviewProvider {
+    static var previews: some View {
+        SectionTitleView(title: "Section Title")
+            .previewLayout(.sizeThatFits)
+    }
+}
