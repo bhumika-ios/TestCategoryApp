5af1499d7bfbf30c1a2fe692bacf0dd810656fe6
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2023-01-20 15:53:28 +0530

    save data for readable data for used in CoreData

--- TestCategoryApp/ModelView/Todo.swift
+++ TestCategoryApp/ModelView/Todo.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import CoreData
+
+extension Todo {
+    static func createFakeTodo(group: Group, context: NSManagedObjectContext? = nil) -> Todo {
+        let newTodo = context != nil ? Todo(context: context!) : Todo()
+        newTodo.group = group
+        newTodo.id = UUID()
+        newTodo.title = "Some Todo"
+        newTodo.createdAt = Date()
+        newTodo.doDate = Date()
+        
+        return newTodo
+    }
+    
+    var readableDoDate: String {
+        get {
+            let df = DateFormatter()
+            df.dateFormat = "MMM dd,yyyy"
+            
+            if (self.doDate != nil) {
+                return df.string(from: self.doDate!)
+            }
+            
+            return ""
+        }
+    }
+    
+    func toggle(context: NSManagedObjectContext) {
+        self.done.toggle()
+        
+        PersistenceController.shared.save(context: context)
+    }
+}
