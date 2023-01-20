e548407a8da2eeeb0b1ca34bd5aa44e8b4e6c9be
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2023-01-20 15:58:58 +0530

    done and undone used list

--- TestCategoryApp/View/CheckBoxView.swift
+++ TestCategoryApp/View/CheckBoxView.swift
@@ -5,4 +5,50 @@
 //  Created by Bhumika Patel on 20/01/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct CheckboxView: View {
+    var label: String?
+    var onToggle: (() -> Void)?
+    
+    @State private var checked = false
+    
+    init (label: String? = nil, defaultChecked: Bool? = false, onToggle: (() -> Void)? = {}) {
+        self.label = label
+        self.onToggle = onToggle
+        _checked = .init(initialValue: defaultChecked ?? false)
+    }
+    
+    var body: some View {
+        HStack {
+            if checked {
+                Image(systemName: "checkmark.square")
+                    .foregroundColor(.indigo)
+            } else {
+                Image(systemName: "square")
+            }
+            
+            if label != nil {
+                Text(label!)
+            }
+        }.onTapGesture {
+            withAnimation {
+                checked.toggle()
+                
+                if onToggle != nil {
+                    (onToggle!)()
+                }
+            }
+        }
+    }
+}
+
+struct CheckboxView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckboxView()
+            .previewLayout(.sizeThatFits)
+        
+        CheckboxView(label: "Some Label")
+            .previewLayout(.sizeThatFits)
+    }
+}
