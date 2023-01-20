5a9be87987c28b4b2e457410b1a58c29822ffb41
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2023-01-20 15:49:42 +0530

    only list wise data

--- TestCategoryApp/Screen/ToDoListData.swift
+++ TestCategoryApp/Screen/ToDoListData.swift
@@ -8,8 +8,33 @@
 import SwiftUI
 
 struct ToDoListData: View {
+    @State private var searchValue = ""
+    @State private var filteredByGroup: Group? = nil
+    @State private var isAddGroupOpen = false
+    @State private var isAddTodoOpen = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView {
+                SectionTitleView(title: "")
+                
+                TodoList(query: searchValue, group: filteredByGroup)
+            }
+        
+        .padding(.leading)
+        .padding(.trailing)
+        .searchable(text: $searchValue, placement: .toolbar)
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem (placement: .primaryAction) {
+                Button(action: { isAddTodoOpen = true }) {
+                    Image(systemName: "plus.circle")
+                        .sheet(isPresented: $isAddTodoOpen) {
+                            PublishTodoScreen()
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
