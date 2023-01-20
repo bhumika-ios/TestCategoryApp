0f20feff3aa36d71c607f42ef6c443cf7e1b9b13
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2023-01-20 15:58:22 +0530

    edit and delete view tapclijck display list wise data

--- TestCategoryApp/View/ListItemView.swift
+++ TestCategoryApp/View/ListItemView.swift
@@ -5,4 +5,64 @@
 //  Created by Bhumika Patel on 20/01/23.
 //
 
-import Foundation
+import SwiftUI
+import CoreData
+
+struct ListItemView: View {
+    @Environment(\.managedObjectContext) private var moc
+    
+    @ObservedObject var todo: Todo
+    
+    @State private var doesClose = false
+    @State private var isEditTodoOpen = false
+    
+    private func deleteTodo(object: NSManagedObject) {
+        PersistenceController.shared.delete(context: moc, object: object)
+    }
+    
+    var body: some View {
+        VStack {
+            HStack {
+                VStack (alignment: .leading, spacing: 5) {
+                    HStack {
+                        CheckboxView(defaultChecked: todo.done, onToggle: {
+                            todo.toggle(context: moc)
+                        })
+                        if todo.title != nil {
+                            Text(todo.title!)
+                                .onTapGesture {
+                                    isEditTodoOpen.toggle()
+                                }
+                                .sheet(isPresented: $isEditTodoOpen) {
+                                    PublishTodoScreen(todo: todo)
+                                }
+                        }
+                    }
+                    
+                    if todo.group != nil {
+                        Text("\(todo.group!.title!) | \(todo.readableDoDate)")
+                            .foregroundColor(Color.gray)
+                            .font(.system(size: 14))
+                    }
+                }
+                
+                Spacer()
+                
+                Image(systemName: "trash")
+                    .onTapGesture {
+                        deleteTodo(object: todo)
+                    }
+            }
+            Divider()
+        }
+    }
+}
+
+let todo = Todo.createFakeTodo(group: Group())
+
+struct ListItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListItemView(todo: todo)
+            .previewLayout(.sizeThatFits)
+    }
+}
