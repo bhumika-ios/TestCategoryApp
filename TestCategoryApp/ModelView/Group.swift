cbe7df69a49d9acb4ef603d977ac342bc0a7aa83
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2023-01-20 15:54:50 +0530

    inbuilt store group in data

--- TestCategoryApp/ModelView/Group.swift
+++ TestCategoryApp/ModelView/Group.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+import CoreData
+
+extension Group {
+    static func createFakeGroup(context: NSManagedObjectContext? = nil) -> Group {
+        let group = context != nil ? Group(context: context!) : Group()
+        group.id = UUID()
+        group.title = "Some Group"
+        group.color = "#2f42d6"
+        group.systemIcon = "list.bullet.circle.fill"
+        
+        return group
+    }
+}
+
